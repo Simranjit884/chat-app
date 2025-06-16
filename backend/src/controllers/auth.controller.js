e024a59c6d6d29938aafd86ffa827f356b25938e
samy884 <ssgill884@gmail.com>
2025-06-16 20:20:11 +0530

    updated auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 export const signup = (req, res) => {
-  res.send("signup route");
+  const { fullName, email, password } = req.body;
 };
 export const login = (req, res) => {
   res.send("login route");
