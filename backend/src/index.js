b21f2979436c0d92558d538a37260c6812b206d8
samy884 <ssgill884@gmail.com>
2025-06-22 16:19:15 +0530

    check auth route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,6 +4,7 @@ import {
   logout,
   signup,
   updateProfile,
+  checkAuth,
 } from "../controllers/auth.controller.js";
 
 import { protectRoute } from "../middleware/auth.middleware.js";
@@ -17,5 +18,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
