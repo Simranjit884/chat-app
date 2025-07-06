b0e28a607735aa918c81075f9a5b50c8f57e2a64
samy884 <ssgill884@gmail.com>
2025-07-06 21:52:23 +0530

    add message route to backend API

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on port:" + PORT);
