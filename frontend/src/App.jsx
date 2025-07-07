3623e20a9c7b74e8ad17aad5cc284b306972d65b
samy884 <ssgill884@gmail.com>
2025-07-07 12:06:09 +0530

    daisy ui setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const App = () => {
-  return <div className="bg-amber-700 text-white">Appsff</div>;
+  return (
+    <div>
+      <button className="btn btn-xs">Xsmall</button>
+      <button className="btn btn-sm">Small</button>
+      <button className="btn">Medium</button>
+      <button className="btn btn-lg">Large</button>
+      <button className="btn btn-xl">Xlarge</button>
+    </div>
+  );
 };
 
 export default App;
