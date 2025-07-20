939988af2117d41f635fb784fbb60a46e501c4c1
samy884 <ssgill884@gmail.com>
2025-07-20 15:52:16 +0530

    implemented routes protection on the frontend side

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,15 +5,22 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
+import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config(); // for
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json()); // for parsing JSON data from req.body into object.
+app.use(cookieParser()); // Parses cookies from incoming requests and puts them into req.cookies
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // Allows cookies or auth headers to be sent/received
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
