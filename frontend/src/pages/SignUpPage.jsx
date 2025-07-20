152763e23a4c927a04360f8b177e8371f606d7f3
samy884 <ssgill884@gmail.com>
2025-07-20 17:06:13 +0530

    implemented left side

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -9,6 +9,7 @@ import {
   MessageSquare,
   User,
 } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState();
@@ -27,17 +28,17 @@ const SignUpPage = () => {
   };
 
   return (
-    <div className="min-h-screen grid lg:grid-cols-2">
+    <div className="grid min-h-screen lg:grid-cols-2">
       {/* left side */}
-      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+      <div className="flex flex-col items-center justify-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
           {/*Logo */}
-          <div className="text-center mb-8">
-            <div className="flex flex-col items-center gap-2 group">
-              <div className="size-12 flex items-center justify-center rounded-xl group-hover:bg-primary/20 transition-colors">
+          <div className="mb-8 text-center">
+            <div className="group flex flex-col items-center gap-2">
+              <div className="group-hover:bg-primary/20 flex size-12 items-center justify-center rounded-xl transition-colors">
                 <MessageSquare />
               </div>
-              <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+              <h1 className="mt-2 text-2xl font-bold">Create Account</h1>
               <p className="text-base-content/60">
                 Get started with your free account
               </p>
@@ -49,22 +50,96 @@ const SignUpPage = () => {
               <label className="label">
                 <span className="label-text font-medium">Full Name</span>
               </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <User className="size-5 text-base-content/40" />
-                </div>
+
+              <label className="input input-bordered flex w-full items-center gap-2">
+                <User className="text-base-content/40 size-5" />
                 <input
                   type="text"
-                  className={`input input-bordered w-full pl-10`}
                   placeholder="John Doe"
+                  className="grow"
                   value={formData.fullName}
                   onChange={(e) =>
                     setFormData({ ...formData, fullName: e.target.value })
                   }
                 />
-              </div>
+              </label>
             </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+
+              <label className="input input-bordered flex w-full items-center gap-2">
+                <Mail className="text-base-content/40 size-5" />
+                <input
+                  type="email"
+                  placeholder="you@example.com"
+                  className="grow"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+              </label>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+
+              <label className="input input-bordered flex w-full items-center gap-2">
+                <Lock className="text-base-content/40 size-5" />
+
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="••••••••"
+                  className="grow"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                />
+
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="text-base-content/40 hover:text-base-content transition-colors"
+                >
+                  {showPassword ? (
+                    <EyeOff className="size-5" />
+                  ) : (
+                    <Eye className="size-5" />
+                  )}
+                </button>
+              </label>
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary w-full"
+              disabled={isSigningUp}
+            >
+              {isSigningUp ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Create Account"
+              )}
+            </button>
           </form>
+
+          <div className="text-center">
+            <p className="text-base-content/60">
+              Already have an account?{" "}
+              <Link to="/login" className="link link-primary">
+                Sign in
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
