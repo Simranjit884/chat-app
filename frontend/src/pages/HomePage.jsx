e03a0c0a8215c6d201411880bac5aa5b183fc679
samy884 <ssgill884@gmail.com>
2025-07-22 18:08:39 +0530

    implemented the initial nochatselected ui

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useChatStore } from "../store/useChatStore";
+
+import Sidebar from "../components/Sidebar";
+import NoChatSelected from "../components/NoChatSelected";
+import ChatContainer from "../components/ChatContainer";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
-};
+  const { selectedUser } = useChatStore();
 
+  return (
+    <div className="bg-base-200 h-screen">
+      <div className="flex items-center justify-center px-4 pt-20">
+        <div className="bg-base-100 shadow-cl h-[calc(100vh-8rem)] w-full max-w-6xl rounded-lg">
+          <div className="flex h-full overflow-hidden rounded-lg">
+            <Sidebar />
+
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default HomePage;
