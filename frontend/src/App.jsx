4359a255f163ef6d7ca5df022929ddeba874ffab
samy884 <ssgill884@gmail.com>
2025-07-22 17:53:49 +0530

    setting up themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,16 +9,24 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
+
+  useEffect(() => {
+    if (theme) {
+      document.documentElement.setAttribute("data-theme", theme); // ✅ Apply to <html>
+    }
+  }, [theme]);
 
   console.log(authUser);
 
@@ -31,7 +39,7 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <NavBar />
       <Routes>
         <Route
