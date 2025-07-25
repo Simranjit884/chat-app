daabf2dd70b4b5116dc43abf3e8c9c9a5d48bd13
samy884 <ssgill884@gmail.com>
2025-07-25 14:13:15 +0530

    integrated socket.io for real-time messaging; updated backend to handle socket connections and emit online user status; modified frontend to manage socket connections and display online users

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,9 @@ import messageRoutes from "./routes/message.route.js";
 
 import cors from "cors";
 import dotenv from "dotenv";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config(); // for
-const app = express();
 
 const PORT = process.env.PORT;
 
@@ -25,7 +25,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server is running on port:" + PORT);
   connectDB();
 });
--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -7,16 +7,19 @@ const ChatHeader = () => {
   const { onlineUsers } = useAuthStore();
 
   return (
-    <div className="border-base-300 border-b p-2.5">
+    <div className="border-base-300 relative border-b p-2.5">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
           {/* Avatar */}
           <div className="avatar">
-            <div className="relative size-10 rounded-full">
+            <div className="z-10 size-10 overflow-visible rounded-full">
               <img
                 src={selectedUser.profilePic || "/avatar.png"}
                 alt={selectedUser.fullName}
               />
+              {onlineUsers.includes(selectedUser._id) && (
+                <span className="absolute top-0.5 right-0 z-100 size-3 rounded-full bg-green-500 ring-2 ring-zinc-900" />
+              )}
             </div>
           </div>
 
